b504a2e246121075cdd6bf3f96be69eeb3218f5b
Alma Ozoria <heisylior@hotmail.com>
2025-06-18 19:47:04 -0400

    Se agrego el ApiModule

--- app/src/main/java/edu/ucne/almarosa_ap2_p2/data/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/almarosa_ap2_p2/data/di/ApiModule.kt
@@ -11,28 +11,26 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
+@Module
 object ApiModule {
-    @InstallIn(SingletonComponent::class)
-    @Module
-    object ApiModule {
-        const val BASE_URL = "https://api.github.com/users/enelramon/repos/"
+    const val BASE_URL = "https://api.github.com/users/enelramon/repos/"
 
-        @Provides
-        @Singleton
-        fun providesMoshi(): Moshi =
-            Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
 
-        @Provides
-        @Singleton
-        fun providesGitHubApi(moshi: Moshi): GitHubApi {
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .build()
-                .create(GitHubApi::class.java)
-        }
-
+    @Provides
+    @Singleton
+    fun providesGitHubApi(moshi: Moshi): GitHubApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(GitHubApi::class.java)
     }
+
 }
