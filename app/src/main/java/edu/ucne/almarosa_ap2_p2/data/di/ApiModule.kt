010c9145d84b4658b891171f2433c91f02896611
Alma Ozoria <heisylior@hotmail.com>
2025-06-18 19:24:07 -0400

    Se agrego el ApiModule

---
+++ app/src/main/java/edu/ucne/almarosa_ap2_p2/data/di/ApiModule.kt
@@ -0,0 +1,38 @@
+package edu.ucne.almarosa_ap2_p2.data.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.almarosa_ap2_p2.presentacion.remote.GitHubApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+object ApiModule {
+    @InstallIn(SingletonComponent::class)
+    @Module
+    object ApiModule {
+        const val BASE_URL = "https://api.github.com/users/enelramon/repos/"
+
+        @Provides
+        @Singleton
+        fun providesMoshi(): Moshi =
+            Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+
+        @Provides
+        @Singleton
+        fun providesGitHubApi(moshi: Moshi): GitHubApi {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+                .create(GitHubApi::class.java)
+        }
+
+    }
+}
