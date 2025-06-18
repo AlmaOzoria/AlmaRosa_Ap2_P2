173bc300581ba0003c4443ba6429226c33502eeb
Alma Ozoria <heisylior@hotmail.com>
2025-06-18 19:47:26 -0400

    Se agrego el ApiRepository

---
+++ app/src/main/java/edu/ucne/almarosa_ap2_p2/data/local/repository/ApiRepository.kt
@@ -0,0 +1,23 @@
+package edu.ucne.almarosa_ap2_p2.data.local.repository
+
+import edu.ucne.almarosa_ap2_p2.presentacion.remote.DataSource
+import edu.ucne.almarosa_ap2_p2.presentacion.remote.Resource
+import edu.ucne.almarosa_ap2_p2.presentacion.remote.dto.RepositoryDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class ApiRepository @Inject constructor(
+        private val dataSource: DataSource
+    ) {
+        fun getApi(): Flow<Resource<List<RepositoryDto>>> = flow {
+            try {
+                emit(Resource.Loading())
+                val Api = dataSource.listRepos()
+                emit(Resource.Success(Api))
+            } catch (e: Exception) {
+                emit(Resource.Error("Error: ${e.message}"))
+            }
+        }
+
+    }
