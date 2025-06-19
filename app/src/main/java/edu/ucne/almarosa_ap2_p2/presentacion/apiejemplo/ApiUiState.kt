a93012db00a1b16eefe623cd9aa2c44b1a42ac3e
Alma Ozoria <heisylior@hotmail.com>
2025-06-18 20:21:38 -0400

    Se creo el ApiUiState

---
+++ app/src/main/java/edu/ucne/almarosa_ap2_p2/presentacion/apiejemplo/ApiUiState.kt
@@ -0,0 +1,13 @@
+package edu.ucne.almarosa_ap2_p2.presentacion.apiejemplo
+
+import edu.ucne.almarosa_ap2_p2.presentacion.remote.dto.RepositoryDto
+
+data class ApiUiState (
+        val name: String = "",
+        val description: String? = null,
+        val htmlUrl: String = "",
+        val isLoading: Boolean = false,
+        val errorMessage: String? = null,
+        val Api: List<RepositoryDto> = emptyList(),
+        val inputError: String? = null
+)
