6ac1c7b1d993443d6a610e50e4cbf51e880213b3
Alma Ozoria <heisylior@hotmail.com>
2025-06-18 20:51:05 -0400

    Se creo el app

--- app/src/main/java/edu/ucne/almarosa_ap2_p2/AlmaRosa_Ap2_P2App.kt
+++ app/src/main/java/edu/ucne/almarosa_ap2_p2/AlmaRosa_Ap2_P2App.kt
@@ -1,4 +1,7 @@
 package edu.ucne.almarosa_ap2_p2
 
-class AlmaRosa_Ap2_P2App {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AlmaRosa_Ap2_P2App: Application()
