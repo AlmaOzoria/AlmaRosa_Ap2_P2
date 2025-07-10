4603c74ee769784628f4ed73f32463c0774c91ab
Alma Ozoria <heisylior@hotmail.com>
2025-07-09 21:18:38 -0400

    se agrego en owner

--- app/src/main/java/edu/ucne/almarosa_ap2_p2/remote/dto/RepositoryDto.kt
+++ app/src/main/java/edu/ucne/almarosa_ap2_p2/remote/dto/RepositoryDto.kt
@@ -2,8 +2,13 @@ package edu.ucne.almarosa_ap2_p2.remote.dto
 
 import com.squareup.moshi.Json
 
+data class OwnerDto(
+    val login: String
+)
+
 data class RepositoryDto(
     @Json(name = "name") val name: String,
     @Json(name = "description") val description: String?,
-    @Json(name = "html_url") val htmlUrl: String
+    @Json(name = "html_url") val htmlUrl: String,
+    val owner: OwnerDto
 )
