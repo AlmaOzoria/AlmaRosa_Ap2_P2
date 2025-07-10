de5daf097cf907abefbbfb87a559428ba490db6e
Alma Ozoria <heisylior@hotmail.com>
2025-07-09 21:18:00 -0400

    se agrego en el contribuideres

--- app/src/main/java/edu/ucne/almarosa_ap2_p2/remote/GitHubApi.kt
+++ app/src/main/java/edu/ucne/almarosa_ap2_p2/remote/GitHubApi.kt
@@ -1,10 +1,19 @@
 package edu.ucne.almarosa_ap2_p2.remote
 
+import edu.ucne.almarosa_ap2_p2.remote.dto.ContribuidorDto
 import edu.ucne.almarosa_ap2_p2.remote.dto.RepositoryDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface GitHubApi {
     @GET("users/enelramon/repos")
     suspend fun listRepos(): Response<List<RepositoryDto>>
+
+    @GET("repos/{owner}/{repo}/contributors")
+    suspend fun listContributors(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): Response<List<ContribuidorDto>>
+
 }
